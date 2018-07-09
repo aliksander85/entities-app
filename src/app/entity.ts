762803f922f500f6fde067b7215c941bac6e76fa
Alexander Potapov <potapov.av.85@gmail.com>
2018-07-09 13:34:18 +0300

    Added mock entities

---
+++ src/app/entity.ts
@@ -0,0 +1,6 @@
+export class Entity {
+  id: number;
+  name: string;
+  category: string;
+  checked?: boolean;
+}
---
+++ src/app/mock-entities.ts
@@ -0,0 +1,9 @@
+import { Entity } from './entity';
+
+export const ENTITIES: Entity[] = [
+  { id: 1, name: 'Arrivals', category: 'BPM' },
+  { id: 2, name: 'Car Wash', category: 'BPM' },
+  { id: 3, name: 'Maintenance', category: 'Project' },
+  { id: 4, name: 'Customer Payment', category: 'BPM' },
+  { id: 5, name: 'Technical issues', category: 'Project' }
+];
